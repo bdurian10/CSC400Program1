37628c91f52a9093c4d81fdbebc2834e47ee28a6
Braden Durian <braden.durian@csuglobal.edu>
2024-04-28 14:25:36 -0600

    Fifth Commit: Moved main method from TestBag to Bag class to provide one java file for assignment submission

--- src/bag/Bag.java
+++ src/bag/Bag.java
@@ -53,5 +53,57 @@ public class Bag<T> {
 	public Map<T, Integer> getItems(){
 		return items;
 	}
+	
+	//Main Method for testing
+	public static void main(String[] args) {
+			
+			Bag<String> words = new Bag<>();
+			
+			//Add strings to bag, including duplicates
+			words.add("Hello");
+			words.add("Hello");
+			words.add("World");
+			words.add("California");
+			words.add("Tabletop");
+			words.add("tabletop");
+			words.add("California");
+			words.add("Hello");
+			
+			//Print items in Bag
+			System.out.println(words.getItems());
+			
+			/*Test contains method
+			 *A more effective test would only execute the print statement
+			 *if the result differed from the expected outcome. 
+			 *I chose to print all results for the purpose of this assignment
+			 */
+			System.out.println("****Testing Contains Method****");
+			System.out.println("Bag contains: Hello. Expected: true. Result: " + words.contains("Hello"));
+			System.out.println("Bag contains: Hotdog. Expected: false. Result: " + words.contains("Hotdog"));
+			System.out.println("Bag contains: California. Expected: true. Result: " + words.contains("California"));
+			System.out.println("Bag contains: Tabletop. Expected: true. Result: " + words.contains("Tabletop"));
+			System.out.println("Bag contains: tabletop. Expected: true. Result: " + words.contains("tabletop"));
+			System.out.println();
+			
+			//Test count method
+			System.out.println("****Testing Count Method****");
+			System.out.println("Count of Hello: " + words.count("Hello"));
+			System.out.println("Count of California: " + words.count("California"));
+			System.out.println("Count of Tabletop: " + words.count("Tabletop"));
+			System.out.println("Count of tabletop: " + words.count("tabletop"));
+			System.out.println("Count of Jet: " + words.count("Jet"));
+			System.out.println();
+			
+			//Test Remove Method
+			System.out.println("****Testing Remove Method****");
+			words.remove("Hello");
+			System.out.println("Bag after removal of Hello: " + words.getItems());
+			System.out.println("Bag contains: Hello. Expected: true. Result: " + words.contains("Hello"));
+			System.out.println("Count of Hello: " + words.count("Hello"));
+			words.remove("Tabletop");
+			System.out.println("Bag after removal of Tabletop: " + words.getItems());
+			System.out.println("Bag contains: Tabletop. Expected: false. Result: " + words.contains("Tabletop"));
+			System.out.println("Count of Tabletop: " + words.count("Tabletop"));
+		}
 
 }
